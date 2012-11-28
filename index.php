80f853bd64dd390cf735e2e51e8da7a6f940bedd
Torrance Scott <torrey.scott@gmail.com>
2012-11-27 18:08:25 -0600

    Original
    Includes basic html 5 template, css sticky footer, reset css

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>HTML5 boilerplate—all you really need…</title>
+	<link rel="stylesheet" href="css/reset.css" />
+	<link rel="stylesheet" href="css/main.css" />
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+    <body>
+        <div class="wrapper">
+            <p>Your website content here.</p>
+            <div class="push"></div>
+        </div>
+        <div class="footer">
+            <p>Copyright (c) 2008</p>
+        </div>
+    </body>
+</html>
